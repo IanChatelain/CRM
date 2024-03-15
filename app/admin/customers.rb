6ca0a51c9d28b6426f62698e9eb184284bef227f
IanChatelain <ichatelain@academic.rrc.ca>
2024-03-15 15:53:44 -0500

    customers form and view working

---
+++ app/admin/customers.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Customer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :full_name, :phone_number, :email, :notes
+
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:full_name, :phone_number, :email, :notes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.actions
+  end
+
+end
+
