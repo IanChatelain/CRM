3679050393754f9bf414a200d939a613a465e31f
IanChatelain <ichatelain@academic.rrc.ca>
2024-03-16 14:23:07 -0500

    customer controller and index generated, partial and image rendering working as intended

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,11 +1,10 @@
 ActiveAdmin.register Customer do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :full_name, :phone_number, :email, :notes
+  permit_params :full_name, :phone_number, :email, :notes, :image
 
   remove_filter :image_attachment, :image_blob
   #
@@ -25,6 +24,4 @@ ActiveAdmin.register Customer do
     end
     f.actions
   end
-
 end
-
