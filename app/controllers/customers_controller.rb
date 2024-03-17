12c5c7ec24f1f41ae55346b750734bb39d0ecffa
IanChatelain <ichatelain@academic.rrc.ca>
2024-03-17 15:05:27 -0500

    Challenge complete - Submission commit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,11 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+  def missing_email
+    # Nice use of arrays here to build the SQL query.
+    @customers = Customer.where.not(email: [nil, ''])
+  end
 end
